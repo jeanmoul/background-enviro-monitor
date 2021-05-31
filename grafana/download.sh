e870e279e300b0c5d8e72deb19cf08c1e4f77c25
root <root@LAPTOP01.localdomain>
2021-05-31 18:45:43 +0200

    bumping version and adding enviro+ from pimoroni

--- grafana/download.sh
+++ grafana/download.sh
@@ -3,10 +3,10 @@
 outfile="/tmp/grafana.deb"
 download_base="https://dl.grafana.com/oss/release/"
 case $1 in
-   rpi)  package_file="grafana-rpi_7.0.3_armhf.deb"
+   rpi)  package_file="grafana-rpi_7.5.7_armhf.deb"
        ;;
-   aarch64) package_file="grafana_7.0.3_arm64.deb"
+   aarch64) package_file="grafana_7.5.7_arm64.deb"
        ;;
-   *) package_file="grafana_7.0.3_armhf.deb"
+   *) package_file="grafana_7.5.7_armhf.deb"
 esac
 wget -O "${outfile}" "${download_base}${package_file}"
